4835bfcefd7e9348d0c31dea1454ab44c041f353
yxy <1634442473@qq.com>
2022-10-27 20:06:11 +0800

    first commit

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author yuxuyao
+ * @date 2022/10/27 - 20:04
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("yuxyaoooooooooooooooooooo");
+    }
+}
